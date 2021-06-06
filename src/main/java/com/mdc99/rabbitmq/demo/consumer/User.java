4f0139f30b556a213c161dc33ca197dcd56424f5
mital <mitalchaudhari247@gmail.com>
2021-06-06 17:06:41 +0530

    initial commit

---
+++ src/main/java/com/mdc99/rabbitmq/demo/consumer/User.java
@@ -0,0 +1,15 @@
+package com.mdc99.rabbitmq.demo.consumer;
+
+import com.mdc99.rabbitmq.demo.config.MessagingConfig;
+import com.mdc99.rabbitmq.demo.dto.OrderStatus;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+
+    @RabbitListener(queues = MessagingConfig.QUEUE)
+    public void consumeMessageFromQueue(OrderStatus orderStatus) {
+        System.out.println("Message recieved from queue : " + orderStatus);
+    }
+}
